7c7072de8158573f23072ae8e8e436c980ecf9ca
har5hit <har5hit@live.com>
2017-05-26 19:27:51 +0530

    Test 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyA48EfHZvqlZkRDaxzWuaxP8DGMzV0sBqg",
+    authDomain: "angular4-demo.firebaseapp.com",
+    databaseURL: "https://angular4-demo.firebaseio.com",
+    projectId: "angular4-demo",
+    storageBucket: "angular4-demo.appspot.com",
+    messagingSenderId: "115835651427"
+  }
 };
